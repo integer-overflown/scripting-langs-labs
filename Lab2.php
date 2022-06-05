2317a2d42e2277a3a7326aac1769f75e65adf678
max-khm <maksym.khomenko@nure.ua>
2022-06-05 13:55:32 +0300

    + Add menu and product selection

--- Lab2.php
+++ Lab2.php
@@ -1,11 +1,81 @@
 <?php
 
-function shutdown(): void {
+$products = [1 => "Coffee", 2 => "Cocoa", 3 => "Tea", 4 => "Cola", 5 => "Pepsi", 6 => "Sprite", 7 => "Juice"];
+
+function shutdown(): void
+{
     echo "Bye!\n";
     exit(0);
 }
 
-$userName = getenv("USER");
+function parseIntegerInput(string $input, int $rangeStart, $rangeEnd): ?int
+{
+    if (!is_numeric($input)) {
+        return null;
+    }
+    $parsed = intval($input);
+    if ($parsed < $rangeStart || $parsed > $rangeEnd) {
+        return null;
+    }
+    return $parsed;
+}
+
+function startShoppingEntry(): void
+{
+    global $products;
+    $numProducts = count($products);
+
+    echo "Here's what we have:\n";
+    foreach ($products as $num => $name) {
+        echo "$num) $name\n";
+    }
+
+    echo "Choose any by typing a number of menu entry\n";
+
+    for (; ;) {
+        $input = readline("product> ");
+
+        if ($input === false) { // end-of-input
+            break;
+        }
+
+        $tokens = preg_split('/\s+/', $input, -1, PREG_SPLIT_NO_EMPTY);
+
+        if (empty($tokens)) {
+            echo "Please enter a number in range [1, $numProducts]\n";
+            continue;
+        }
+
+        $productIndex = parseIntegerInput($tokens[0], 0, $numProducts); // '0' is allowed as exit request
+        $productCount = 1;
+
+        if ($productIndex === null) {
+            echo "Please enter a valid product index in range [1, $numProducts] (cannot recognize '" . $tokens[0] . "')\n";
+            continue;
+        }
+
+        if ($productIndex == 0) {
+            break;
+        }
+
+        if (count($tokens) > 1) {
+            $productCount = parseIntegerInput($tokens[1], 1, $numProducts);
+
+            if ($productCount === null) {
+                echo "Please enter a valid product count, should be an integer larger than 1 (cannot recognize '" . $tokens[1] . "')\n";
+                continue;
+            }
+        }
+
+        $chosenProduct = $products[$productIndex];
+
+        echo "You've chosen '$chosenProduct' in amount of $productCount\n";
+    }
+
+    echo "Done shopping\n";
+}
+
+$userName = getenv("USER"); // request username from USER environment variable (is there on all *nix systems)
 
 if (!$userName) {
     $userName = "user";
@@ -35,6 +105,9 @@ do {
             shutdown();
             break;
         case "1":
+            echo "-- Start shopping --\n";
+            startShoppingEntry();
+            break;
         case "2":
         case "3":
             $invalidInput = false;
